a0d332e56ba3c455c65be10bec98195e705e31c1
Aksh <soneji.akash427@gmail.com>
2017-08-30 16:44:20 +0530

    Redirection Protocol added
    Redirection protocol for XIB as well as Storyboard 
    i.e - storyboard and XIB
            let detail = SecondStoryController.getViewController()
            navigationController?.pushViewController(detail, animated: true)

--- Clone/ViewController.swift
+++ Clone/ViewController.swift
@@ -290,6 +290,81 @@ struct myNotification {
 }
 
 
+
+
+
+protocol RedirectionProtocol {
+    
+    static func getViewController() -> UIViewController
+    
+}
+
+protocol XIBRedirectionProtocol: RedirectionProtocol {
+    
+    static var nibIdentifier: String { get }
+    
+}
+
+extension XIBRedirectionProtocol where Self: UIViewController {
+    
+    static var nibIdentifier: String {
+        return String(describing: self)
+    }
+    
+    private static func fromNib() -> Self {
+        let viewController = Self(nibName: nibIdentifier, bundle:nil)
+        return viewController
+    }
+    
+    static func getViewController() -> UIViewController {
+        return fromNib()
+    }
+    
+}
+
+
+
+
+
+
+protocol StoryboardRedirectionProtocol: RedirectionProtocol {
+
+    static var storyboard: UIStoryboard { get }
+    
+    static var storyboardIdentifier: String { get }
+
+}
+
+
+extension StoryboardRedirectionProtocol where Self: UIViewController {
+
+    
+    static var storyboard: UIStoryboard {
+        return UIStoryboard(name: "Storyboard", bundle: nil)
+    }
+    
+    
+    static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+    
+
+    private static func fromStoryboard() -> Self {
+
+        let viewController = storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
+        return viewController
+    }
+
+    static func getViewController() -> UIViewController {
+        return fromStoryboard()
+    }
+
+}
+
+
+
+
+
 typealias RESPONSEJSON = [String: Any]
 
 class Session {
