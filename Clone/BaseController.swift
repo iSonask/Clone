8451e46da6b3575b8c6b53ef9823e8f52089dc1a
Aksh <soneji.akash427@gmail.com>
2017-10-31 10:14:11 +0530

    Update BaseController.swift
    round corner manually. for anu UIbutton , uiview

--- Clone/BaseController.swift
+++ Clone/BaseController.swift
@@ -202,3 +202,48 @@ public extension UIViewController {
 }
 
 //USAGE - containerViewController.show
+
+
+
+
+
+extension UIView {
+    
+    /**
+     Rounds the given set of corners to the specified radius
+     
+     - parameter corners: Corners to round
+     - parameter radius:  Radius to round to
+     */
+    func round(corners: UIRectCorner, radius: CGFloat) {
+        _round(corners: corners, radius: radius)
+    }
+    
+    /**
+     Rounds the given set of corners to the specified radius with a border
+     
+     - parameter corners:     Corners to round
+     - parameter radius:      Radius to round to
+     - parameter borderColor: The border color
+     - parameter borderWidth: The border width
+     */
+    func round(corners: UIRectCorner, radius: CGFloat, borderColor: UIColor, borderWidth: CGFloat) {
+        let mask = _round(corners: corners, radius: radius)
+        addBorder(mask: mask, borderColor: borderColor, borderWidth: borderWidth)
+    }
+    
+    /**
+     Fully rounds an autolayout view (e.g. one with no known frame) with the given diameter and border
+     
+     - parameter diameter:    The view's diameter
+     - parameter borderColor: The border color
+     - parameter borderWidth: The border width
+     */
+    func fullyRound(diameter: CGFloat, borderColor: UIColor, borderWidth: CGFloat) {
+        layer.masksToBounds = true
+        layer.cornerRadius = diameter / 2
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor;
+    }
+    
+}
