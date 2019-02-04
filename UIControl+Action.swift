9a636d520ad2acd1eff093e368a5822e51a597ec
Aksh <soneji.akash427@gmail.com>
2019-02-04 10:30:00 +0530

    created UIControl+Action
    Adding action at easy with closure

---
+++ UIControl+Action.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+import UIKit
+
+class ClosureSleeve {
+    let closure: ()->()
+    
+    init (_ closure: @escaping ()->()) {
+        self.closure = closure
+    }
+    
+    @objc func invoke () {
+        closure()
+    }
+}
+
+
+extension UIControl {
+    
+    func addAction(for controlEvents: UIControl.Event, _ closure: @escaping ()->()) {
+        let sleeve = ClosureSleeve(closure)
+        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+        objc_setAssociatedObject(self, String(format: "[%d]", arc4random()), sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+}
