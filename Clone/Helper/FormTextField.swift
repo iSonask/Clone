e741fd29416ffcb2bcc258b4c74944072c1992fd
Aksh <soneji.akash427@gmail.com>
2019-02-11 11:17:58 +0530

    New updated
     with bottom line and placeholder color configuration

--- Clone/Helper/FormTextField.swift
+++ Clone/Helper/FormTextField.swift
@@ -8,223 +8,181 @@
 
 import UIKit
 
-@IBDesignable
+import UIKit
+
+struct FormTextFieldConfiguration {
+    
+    public let hightlightColor: UIColor =  UIColor(red: 208.0/255.0, green: 2.0/255.0, blue: 205.0/255.0, alpha: 1.00)
+    public let barDefaultColor: UIColor = .darkGray
+    public let animationDuration: TimeInterval = 0.2
+    public let textFieldHeight: CGFloat = 40.0
+    public let textFieldFont: UIFont = UIFont.systemFont(ofSize: 18.0)
+    public let minimizedPlaceholderFont: UIFont = UIFont.systemFont(ofSize: 10.0)
+}
+
 class FormTextField: UITextField {
     
-    fileprivate let placeHolderLabel: UILabel = UILabel()
-    
-    fileprivate var placeHolderHeight: CGFloat {
-        return placeHolderLabel.font!.lineHeight + 5
+    private struct Constant {
+        static let defaultBottomLineHeight: CGFloat = 1.0
+        static let highlightedBottomLineHeight: CGFloat = 1.0
     }
     
-    override var textAlignment: NSTextAlignment {
-        didSet{
-            placeHolderLabel.textAlignment = textAlignment
+    private var _configuration: FormTextFieldConfiguration?
+    public var configuration: FormTextFieldConfiguration! {
+        set {
+            _configuration = newValue
+        }
+        get {
+            return _configuration ?? FormTextFieldConfiguration()
         }
     }
     
-    override var text: String? {
-        didSet{
-            layoutPlaceHolderLabel()
-        }
-    }
+    private let bottomLine = UIView()
+    private var bottomLineHeightConstraint: NSLayoutConstraint?
+    private let minimizedPlaceholderLabel = UILabel()
     
-    @IBInspectable override var placeholder: String? {
-        get{
-            return placeHolderLabel.text
-        }
-        set{
-            placeHolderLabel.text = newValue
-        }
-    }
-    
-    var placeHolderColor: UIColor = .gray {
-        didSet{
-            placeHolderLabel.textColor = placeHolderColor
-        }
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
+
+    init(configuration: FormTextFieldConfiguration) {
+        
+        super.init(frame: .zero)
+        
+        self.configuration = configuration
+        
+        setupInterface()
     }
     
     required init?(coder aDecoder: NSCoder) {
+        
         super.init(coder: aDecoder)
-        commonInit()
+        
+        setupInterface()
     }
     
-    fileprivate func commonInit() {
+    override var intrinsicContentSize: CGSize {
         
+        let superSize = super.intrinsicContentSize
+        return CGSize(width: superSize.width, height: configuration.textFieldHeight)
+    }
+
+    // MARK: - Interface
+    
+    private func setupInterface() {
+        
+        delegate = self
+        
+        font = configuration.textFieldFont
         borderStyle = .none
-        tintColor = UIColor.black
-        textColor = UIColor.black
-        font = UIFont.systemFont(ofSize: 15)
         
-        // Place holder label
-        placeHolderLabel.translatesAutoresizingMaskIntoConstraints = false
-        placeHolderLabel.textAlignment  = textAlignment
-        placeHolderLabel.textColor  = placeHolderColor
-        addSubview(placeHolderLabel)
+        // bottom line
         
-        var heightConstraint: NSLayoutConstraint?
+        bottomLine.backgroundColor = configuration.barDefaultColor
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bottomLine)
         
-        for constraint in constraints {
-            if constraint.firstAttribute == .height {
-                heightConstraint = constraint
-                heightConstraint?.constant = 50
-            }
+        bottomLineHeightConstraint = bottomLine.heightAnchor.constraint(equalToConstant: 1.0)
+        bottomLineHeightConstraint?.isActive = true
+        
+        bottomLine.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        bottomLine.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        // placeholder
+        
+        minimizedPlaceholderLabel.font = configuration.minimizedPlaceholderFont
+        minimizedPlaceholderLabel.textColor = configuration.hightlightColor
+        minimizedPlaceholderLabel.backgroundColor = .clear
+        minimizedPlaceholderLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // MARK: - PlaceholderLabel animations
+    
+    // MARK: Bottom line
+    
+    private func highlightBottomLine() {
+        
+        layoutIfNeeded()
+        
+        UIView.animate(withDuration: configuration.animationDuration) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.bottomLine.backgroundColor = strongSelf.configuration.hightlightColor
+            strongSelf.bottomLineHeightConstraint?.constant = Constant.defaultBottomLineHeight
+            strongSelf.layoutIfNeeded()
         }
+    }
+    
+    private func resetBottomLine() {
         
-        if heightConstraint == nil {
-            heightConstraint = NSLayoutConstraint(item: self,
-                                                  attribute: .height,
-                                                  relatedBy: .equal,
-                                                  toItem: nil,
-                                                  attribute: .notAnAttribute,
-                                                  multiplier: 1,
-                                                  constant: 50)
-            addConstraint(heightConstraint!)
+        UIView.animate(withDuration: configuration.animationDuration) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.bottomLine.backgroundColor = strongSelf.configuration.barDefaultColor
+            strongSelf.bottomLineHeightConstraint?.constant = Constant.defaultBottomLineHeight
         }
-        
-        
-        //        drawBottomLine()
-        layoutPlaceHolderLabel()
-        
-        // Add observer
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(textFieldDidBeginEditing), name: .UITextFieldTextDidBeginEditing, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(textFieldDidEndEditing), name: .UITextFieldTextDidEndEditing, object: nil)
-        
     }
     
-    deinit {
+    // MARK: Minimized placeholder
+    
+    private func showMinimizedPlaceholderAnimation() {
         
-        // Remove observer
-        NotificationCenter.default.removeObserver(self)
+        minimizedPlaceholderLabel.text = placeholder?.uppercased()
+        addSubview(minimizedPlaceholderLabel)
         
-    }
-    
-    // MARK: - override methods
-    
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-        var rect = super.textRect(forBounds: bounds)
-        rect.origin.y += placeHolderHeight
-        rect.size.height -= placeHolderHeight
-        rect.origin.x += 8
-        rect.size.width -= 8
+        let topConstraint = minimizedPlaceholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10.0)
+        topConstraint.isActive = true
         
-        return rect
-    }
-    
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        var rect = super.editingRect(forBounds: bounds)
-        rect.origin.y += placeHolderHeight
-        rect.size.height -= placeHolderHeight
-        rect.origin.x += 8
-        rect.size.width -= 8
+        minimizedPlaceholderLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        minimizedPlaceholderLabel.alpha = 0.3
         
-        return rect
-    }
-    
-    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = CGRect(x: 0, y: frame.size.height - 22 - 5, width: 22, height: 22)
-        return rect
-    }
-    
-    // MARK: - Notification observer
-    
-    func textFieldDidBeginEditing() {
-        layoutPlaceHolderLabel()
-    }
-    
-    func textFieldDidEndEditing() {
-        layoutPlaceHolderLabel()
-    }
-    
-    // MARK: - private methods
-    
-    fileprivate func layoutPlaceHolderLabel() {
+        layoutIfNeeded()
         
-        placeHolderLabel.alpha = 0
-        
-        placeHolderLabel.removeFromSuperview()
-        addSubview(placeHolderLabel)
-        
-        if isEditing || text!.characters.count > 0 {
-            
-            placeHolderLabel.font = UIFont.systemFont(ofSize: 15)
-            
-            var leftMargin: CGFloat = 8
-            var rightMargin: CGFloat = 8
-            
-            if let leftView = leftView{
-                leftMargin += leftView.frame.size.width
-            }
-            
-            if let rightView = rightView{
-                rightMargin += rightView.frame.size.width
-            }
-            
-            let metrics: [String : Any] = [
-                "leftMargin" : leftMargin,
-                "rightMargin" : rightMargin,
-                "lineHeight" : placeHolderHeight,
-                ]
-            
-            let views: [String : Any] = [
-                "placeHolderLabel" : placeHolderLabel
-            ]
-            
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-leftMargin-[placeHolderLabel]-rightMargin-|", options: [], metrics: metrics, views: views))
-            
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(==5)-[placeHolderLabel(lineHeight)]-(>=5)-|", options: [], metrics: metrics, views: views))
-            layoutIfNeeded()
-            
-            UIView.animate(withDuration: 0.2, animations: {
-                self.placeHolderLabel.alpha = 1
-            })
-            
-        }else {
-            
-            placeHolderLabel.font = font
-            
-            var leftMargin: CGFloat = 8
-            var rightMargin: CGFloat = 8
-            
-            if let leftView = leftView{
-                leftMargin += leftView.frame.size.width
-            }
-            
-            if let rightView = rightView{
-                rightMargin += rightView.frame.size.width
-            }
-            
-            let metrics: [String : Any] = [
-                "leftMargin" : leftMargin,
-                "rightMargin" : rightMargin,
-                "lineHeight" : placeHolderHeight,
-                ]
-            
-            let views: [String : Any] = [
-                "placeHolderLabel" : placeHolderLabel
-            ]
-            
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-leftMargin-[placeHolderLabel]-rightMargin-|", options: [], metrics: metrics, views: views))
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(>=5)-[placeHolderLabel(lineHeight)]-(==5)-|", options: [], metrics: metrics, views: views))
-            
-            layoutIfNeeded()
-            
-            UIView.animate(withDuration: 0.2, animations: {
-                self.placeHolderLabel.alpha = 1
-            })
-            
+        UIView.animate(withDuration: configuration.animationDuration) { [weak self] in
+            self?.minimizedPlaceholderLabel.alpha = 1.0
+            topConstraint.constant = -5.0
+            self?.layoutIfNeeded()
         }
-        
     }
     
+    private func hideMinimizedPlaceholderAnimation() {
+        
+        UIView.animate(withDuration: configuration.animationDuration, animations: { [weak self] in
+            self?.minimizedPlaceholderLabel.alpha = 0.0
+        }) { [weak self] isFinished in
+            guard true == isFinished else { return }
+            self?.minimizedPlaceholderLabel.removeFromSuperview()
+        }
+    }
 }
 
+extension FormTextField: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let currentText = textField.text ?? ""
+        
+        if currentText.isEmpty {
+            showMinimizedPlaceholderAnimation()
+        } else if currentText.count == 1 && string.isEmpty {
+            hideMinimizedPlaceholderAnimation()
+        }
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        highlightBottomLine()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        resetBottomLine()
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        
+        hideMinimizedPlaceholderAnimation()
+        return true
+    }
+}
 // MARK: - public methods
 
 extension FormTextField{
